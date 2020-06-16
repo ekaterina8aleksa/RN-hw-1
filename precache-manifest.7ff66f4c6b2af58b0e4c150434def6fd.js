0fb206fff46ad3b3ba7141673021ca4f4e2d3681
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-06-16 11:53:21 +0300

    Updates

---
+++ precache-manifest.7ff66f4c6b2af58b0e4c150434def6fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a6a6eb4b5bd51992a9fb7e08044157",
+    "url": "/RN-hw-1/index.html"
+  },
+  {
+    "revision": "8ef5d5cf1f45e7cc074c",
+    "url": "/RN-hw-1/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "bd04ca2f6c23ef4fc5b8",
+    "url": "/RN-hw-1/static/css/main.17c0d35d.chunk.css"
+  },
+  {
+    "revision": "8ef5d5cf1f45e7cc074c",
+    "url": "/RN-hw-1/static/js/2.c6c7c053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RN-hw-1/static/js/2.c6c7c053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd04ca2f6c23ef4fc5b8",
+    "url": "/RN-hw-1/static/js/main.2c8b854c.chunk.js"
+  },
+  {
+    "revision": "2ca183b2ecd9591b661f",
+    "url": "/RN-hw-1/static/js/runtime-main.0fdfd144.js"
+  }
+]);
